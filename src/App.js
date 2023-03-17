78cdf9beafeb44b04b073d21b0a9d2f2b814c6e1
June <june20003@naver.com>
2023-03-17 13:31:39 +0900

    Add nav bar in web page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
+import Title from "./components/Title";
 
 function App() {
   return (
     <Router>
+      <Title />
       <Switch>
         <Route path="/movie/:id">
           <Detail />
---
+++ src/components/Title.js
@@ -0,0 +1,38 @@
+import styles from "./Title.module.css";
+import logo from "../images/logo.png";
+
+function Title({}) {
+  return (
+    <div className={styles.title}>
+      <a href="#">
+        <div className={styles.logo}>
+          <img src={logo} alt="LG로고" className={styles.image}></img>
+          <header>Reskilling Movie</header>
+        </div>
+      </a>
+      <div className={styles.banner}>
+        <a href="#" className={styles.option}>
+          멤버십
+        </a>
+        <a href="#" className={styles.option}>
+          영화관 찾기
+        </a>
+        <a href="https://www.cgv.co.kr/" className={styles.option}>
+          CGV
+        </a>
+        <a href="https://www.megabox.co.kr/" className={styles.option}>
+          메가박스
+        </a>
+        <a href="https://www.lottecinema.co.kr/" className={styles.option}>
+          롯데시네마
+        </a>
+        <div className={styles.option}>
+          <input placeholder="Search Movie"></input>
+          <button>검색</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Title;
